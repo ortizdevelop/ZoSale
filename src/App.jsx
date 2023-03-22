4ea23718f1913a0a3934365e675935fc0335427a
ORTIZ <aidenortiz@mail.ru>
2023-03-23 02:48:19 +0300

    Creating a repository for the ZoSale project.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// Import libraries
+import React from "react";
+
+// Import components
+
+// Import styles (CSS)
+import "./styles/css/nullstyle.css";
+import "./styles/css/App.css";
+
+// Import images (png, jpg, SVG)
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
