c2f8d989cd4e122d52c892762f4b909883ec4a23
ORTIZ <aidenortiz@mail.ru>
2023-03-27 17:23:05 +0300

    Version - 1.2.2. Work on the component Feedback.jsx.
    js/jsx - In the main tags, content was added to work with the project. Images from the images folder were imported.
    
    css/scss - The main style options for the component were applied.

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -5,34 +5,58 @@ import { motion } from "framer-motion";
 import styles from "./Feedback.module.css";
 
 // Images
+import arrow from "./images/arrow.png";
+import bg from "./images/bg.png";
+import icon1 from "./images/icon1.png";
+import icon2 from "./images/icon2.png";
+import icon3 from "./images/icon3.png";
+
 function Feedback() {
   return (
     <div>
       <section className={styles.feedback}>
         <article className={styles.feedback_info}>
           <div>
-            <p className={styles.feedback_info_title}></p>
-            <p className={styles.feedback_info_subtitle}></p>
+            <p className={styles.feedback_info_title}>
+              Close Feedback loop with a public Product Changelog
+            </p>
+            <p className={styles.feedback_info_subtitle}>
+              You can collect feedback manually or by using one of our
+              integrations. Every customer-facing team member should capture the
+              feedback they hear.
+            </p>
           </div>
           <div className={styles.feedback_info_images}>
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img src={arrow} alt="" />
+            <img src={bg} alt="" />
           </div>
         </article>
         <article className={styles.feedback_feature}>
           <ul className={styles.feedback_feature_column}>
             <li className={styles.feedback_feature_list}>
-              <img className={styles.feedback_feature_image} src="" alt="" />
+              <img
+                className={styles.feedback_feature_image}
+                src={icon1}
+                alt=""
+              />
               <p className={styles.feedback_feature_title}></p>
               <p className={styles.feedback_feature_subtitle}></p>
             </li>
             <li className={styles.feedback_feature_list}>
-              <img className={styles.feedback_feature_image} src="" alt="" />
+              <img
+                className={styles.feedback_feature_image}
+                src={icon2}
+                alt=""
+              />
               <p className={styles.feedback_feature_title}></p>
               <p className={styles.feedback_feature_subtitle}></p>
             </li>
             <li className={styles.feedback_feature_list}>
-              <img className={styles.feedback_feature_image} src="" alt="" />
+              <img
+                className={styles.feedback_feature_image}
+                src={icon3}
+                alt=""
+              />
               <p className={styles.feedback_feature_title}></p>
               <p className={styles.feedback_feature_subtitle}></p>
             </li>
