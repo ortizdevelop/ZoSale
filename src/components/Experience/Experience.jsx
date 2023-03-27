d54d6a7da43438d95a58b2aab21abd5fd70706fa
ORTIZ <aidenortiz@mail.ru>
2023-03-27 15:59:28 +0300

    Version 1.1.2 - Completed work on the Experience.jsx component.
    js/jsx - A component Animation.jsx was created, with storing variables for animations. Animation variables were added to the Experience.jsx component. Animations were added, using the library for the main tags.

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,9 @@
 // Libraries
 import { motion } from "framer-motion";
 
+// Components
+import "../Animations/Animations";
+
 // Styles
 import styles from "./Experience.module.css";
 
@@ -9,44 +12,176 @@ import bg from "./images/Image.png";
 import icon1 from "./images/icon1.svg";
 import icon2 from "./images/icon2.svg";
 
+const bounceDown = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const bounceUp = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const bounce = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const bounce2 = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: time * 0.4,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const fadeLeft = {
+  hidden: {
+    x: -50,
+    opacity: 0,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const fadeRight = {
+  hidden: {
+    x: 50,
+    opacity: 0,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
 function Experience() {
   return (
     <div className={styles.experience}>
       <article className={styles.experience_content}>
-        <p className={styles.experience_title}>
+        <motion.p
+          variants={bounceDown}
+          custom={1}
+          className={styles.experience_title}
+        >
           Improve elementor page building experience
-        </p>
-        <p className={styles.experience_subtitle}>
+        </motion.p>
+        <motion.p
+          variants={bounceDown}
+          custom={2}
+          className={styles.experience_subtitle}
+        >
           Add power to your page builder using our easy-to-use and creative
           elements and make your next WordPress pages look even better.
-        </p>
+        </motion.p>
         <ul className={styles.experience_column}>
           <li className={styles.experience_list}>
-            <img className={styles.experience_list_icon} src={icon1} alt="" />
-            <p className={styles.experience_list_title}>
+            <motion.img
+              custom={2}
+              variants={bounce}
+              className={styles.experience_list_icon}
+              src={icon1}
+              alt=""
+            />
+            <motion.p
+              custom={3}
+              variants={fadeLeft}
+              className={styles.experience_list_title}
+            >
               Use our pre-made sections
-            </p>
-            <p className={styles.experience_list_subtitle}>
+            </motion.p>
+            <motion.p
+              custom={4}
+              variants={bounceDown}
+              className={styles.experience_list_subtitle}
+            >
               You can copy any design from the Sark demo site to your site. The
               magic is that you can copy from the frontend to the Element Edit
               panel.
-            </p>
+            </motion.p>
           </li>
           <li className={styles.experience_list}>
-            <img className={styles.experience_list_icon} src={icon2} alt="" />
-            <p className={styles.experience_list_title}>
+            <motion.img
+              custom={2}
+              variants={bounce}
+              className={styles.experience_list_icon}
+              src={icon2}
+              alt=""
+            />
+            <motion.p
+              custom={3}
+              variants={fadeLeft}
+              className={styles.experience_list_title}
+            >
               Customize each element the way you want
-            </p>
-            <p className={styles.experience_list_subtitle}>
+            </motion.p>
+            <motion.p
+              custom={4}
+              variants={bounceDown}
+              className={styles.experience_list_subtitle}
+            >
               Each element comes with a bunch of options to control every
               possible thing. You can achieve nearly any design with your
               imagination.
-            </p>
+            </motion.p>
           </li>
         </ul>
       </article>
       <article className={styles.experience_images}>
-        <img src={bg} alt="" />
+        <motion.img custom={5} variants={fadeRight} src={bg} alt="" />
       </article>
     </div>
   );
