6175ad97e010389eb6fa59aa7eadc1b33afe7046
ORTIZ <aidenortiz@mail.ru>
2023-03-27 17:18:55 +0300

    Version 1.2.1. Work on the Feedback.jsx component.
    js/jsx - In the component, were added basic tags to work. To the tags were added as classes, css modules.
    
    css/scss - In the style files, the main classes and their subclasses were added to work with styles.

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -8,9 +8,36 @@ import styles from "./Feedback.module.css";
 function Feedback() {
   return (
     <div>
-      <section>
-        <article></article>
-        <article></article>
+      <section className={styles.feedback}>
+        <article className={styles.feedback_info}>
+          <div>
+            <p className={styles.feedback_info_title}></p>
+            <p className={styles.feedback_info_subtitle}></p>
+          </div>
+          <div className={styles.feedback_info_images}>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </article>
+        <article className={styles.feedback_feature}>
+          <ul className={styles.feedback_feature_column}>
+            <li className={styles.feedback_feature_list}>
+              <img className={styles.feedback_feature_image} src="" alt="" />
+              <p className={styles.feedback_feature_title}></p>
+              <p className={styles.feedback_feature_subtitle}></p>
+            </li>
+            <li className={styles.feedback_feature_list}>
+              <img className={styles.feedback_feature_image} src="" alt="" />
+              <p className={styles.feedback_feature_title}></p>
+              <p className={styles.feedback_feature_subtitle}></p>
+            </li>
+            <li className={styles.feedback_feature_list}>
+              <img className={styles.feedback_feature_image} src="" alt="" />
+              <p className={styles.feedback_feature_title}></p>
+              <p className={styles.feedback_feature_subtitle}></p>
+            </li>
+          </ul>
+        </article>
       </section>
     </div>
   );
