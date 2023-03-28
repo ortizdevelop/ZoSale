7e477fbab5c6cf2c26bcaaff6f98c86c3ea70f76
ORTIZ <aidenortiz@mail.ru>
2023-03-28 15:38:08 +0300

    Version 1.2.3. The Feedback.jsx component is now complete.
    js/jsx - The Framer-motion library was used. Animations have been added to the main tags, animations got basic settings from variables. The animation takes place when the content is scrolling.
    
    css/scss - Basic css style properties, positioning, flex and grid elements were added. With pseudo elements, animations were added when hovering over the content.

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -11,59 +11,227 @@ import icon1 from "./images/icon1.png";
 import icon2 from "./images/icon2.png";
 import icon3 from "./images/icon3.png";
 
+const bounceDown = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const bounceUp = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const bounce = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const bounce2 = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: time * 0.4,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const fadeLeft = {
+  hidden: {
+    x: -50,
+    opacity: 0,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
+const fadeRight = {
+  hidden: {
+    x: 50,
+    opacity: 0,
+  },
+  visible: (time) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: time * 0.2,
+      duration: 1,
+      ease: "easeInOut",
+    },
+  }),
+};
+
 function Feedback() {
   return (
-    <div>
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{
+        amount: 0.2,
+        once: true,
+      }}
+    >
       <section className={styles.feedback}>
         <article className={styles.feedback_info}>
           <div>
-            <p className={styles.feedback_info_title}>
+            <motion.p
+              variants={bounceDown}
+              custom={1}
+              className={styles.feedback_info_title}
+            >
               Close Feedback loop with a public Product Changelog
-            </p>
-            <p className={styles.feedback_info_subtitle}>
+            </motion.p>
+            <motion.p
+              variants={bounceDown}
+              custom={2}
+              className={styles.feedback_info_subtitle}
+            >
               You can collect feedback manually or by using one of our
               integrations. Every customer-facing team member should capture the
               feedback they hear.
-            </p>
+            </motion.p>
           </div>
           <div className={styles.feedback_info_images}>
-            <img src={arrow} alt="" />
-            <img src={bg} alt="" />
+            <motion.img variants={bounce} custom={3} src={arrow} alt="" />
+            <motion.img variants={fadeRight} custom={4} src={bg} alt="" />
           </div>
         </article>
         <article className={styles.feedback_feature}>
           <ul className={styles.feedback_feature_column}>
-            <li className={styles.feedback_feature_list}>
-              <img
+            <motion.li
+              variants={bounce}
+              custom={5}
+              className={styles.feedback_feature_list}
+            >
+              <motion.img
+                custom={6}
+                variants={bounceDown}
                 className={styles.feedback_feature_image}
                 src={icon1}
                 alt=""
               />
-              <p className={styles.feedback_feature_title}></p>
-              <p className={styles.feedback_feature_subtitle}></p>
-            </li>
-            <li className={styles.feedback_feature_list}>
-              <img
+              <motion.p
+                variants={bounceDown}
+                custom={7}
+                className={styles.feedback_feature_title}
+              >
+                Notify users
+              </motion.p>
+              <motion.p
+                variants={bounceDown}
+                custom={8}
+                className={styles.feedback_feature_subtitle}
+              >
+                Notify users about new product updates, so you can keep them
+                engaged.
+              </motion.p>
+            </motion.li>
+            <motion.li
+              variants={bounce}
+              custom={6}
+              className={styles.feedback_feature_list}
+            >
+              <motion.img
+                custom={7}
+                variants={bounceDown}
                 className={styles.feedback_feature_image}
                 src={icon2}
                 alt=""
               />
-              <p className={styles.feedback_feature_title}></p>
-              <p className={styles.feedback_feature_subtitle}></p>
-            </li>
-            <li className={styles.feedback_feature_list}>
-              <img
+              <motion.p
+                custom={8}
+                variants={bounceDown}
+                className={styles.feedback_feature_title}
+              >
+                Embed anywhere
+              </motion.p>
+              <motion.p
+                custom={9}
+                variants={bounceDown}
+                className={styles.feedback_feature_subtitle}
+              >
+                Embed in-app widget anywhere in your product dashboard or
+                website.
+              </motion.p>
+            </motion.li>
+            <motion.li
+              variants={bounce}
+              custom={7}
+              className={styles.feedback_feature_list}
+            >
+              <motion.img
+                custom={8}
+                variants={bounceDown}
                 className={styles.feedback_feature_image}
                 src={icon3}
                 alt=""
               />
-              <p className={styles.feedback_feature_title}></p>
-              <p className={styles.feedback_feature_subtitle}></p>
-            </li>
+              <motion.p
+                variants={bounceDown}
+                custom={9}
+                className={styles.feedback_feature_title}
+              >
+                Schedule Releases
+              </motion.p>
+              <motion.p
+                custom={10}
+                variants={bounceDown}
+                className={styles.feedback_feature_subtitle}
+              >
+                Schedule your chamgelogs about new feature update or product
+                releases.
+              </motion.p>
+            </motion.li>
           </ul>
         </article>
       </section>
-    </div>
+    </motion.div>
   );
 }
 
