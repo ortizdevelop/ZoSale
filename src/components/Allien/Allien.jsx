84e05eb283a8151cf9c0855bd3d5f5ac7f3f110f
ORTIZ <aidenortiz@mail.ru>
2023-03-30 14:09:04 +0300

    The version is 1.3.1. Work on the component Allien.jsx.
    js/jsx - The main content for the component was added. Basic tags were added and BEM classes were added to them. Images, a styles file and the Framer-Motion library were imported. The use of css modules was eliminated.
    
    css/scss - Normal css files have been added to all components. Refusal of further work with CSS modules in favor of BEM, because of greater convenience and speed of writing code. For the component Allien.jsx, the basic page styles were set.

--- src/components/Allien/Allien.jsx
+++ src/components/Allien/Allien.jsx
@@ -1,3 +1,13 @@
+// Libraries
+import { motion } from "framer-motion";
+
+// CSS Styles
+import "./css/Allien.css";
+
+// Images
+import img1 from "./images/image.png";
+import img2 from "./images/icon.png";
+
 const bounceDown = {
   hidden: {
     opacity: 0,
@@ -96,9 +106,64 @@ const fadeRight = {
 
 function Allien() {
   return (
-    <div>
-      <section></section>
-    </div>
+    <section className="allien">
+      <div className="allien__inner">
+        <article className="allien-top">
+          <img src={img1} alt="" className="allien-top__img" />
+          <ul className="allien-top__column">
+            <li className="allien-top__item">
+              <p className="allien-top__title">
+                Allien uses a modern server-side templating languages
+              </p>
+            </li>
+            <li className="allien-top__item">
+              <p className="allien-top__subtitle">
+                Here are some templating languages that you can use, with little
+                code snippets to show you how to work and what makes them unique
+                & the modern server-side templating language renders directly/
+              </p>
+            </li>
+            <li className="allien-top__item">
+              <button className="allien-top__button">
+                <span>Try For Free</span>
+              </button>
+            </li>
+          </ul>
+        </article>
+        <article className="allien-bottom">
+          <div>
+            <p className="allien-bottom__title">
+              Build a website based on your requirement
+            </p>
+            <img src={img2} alt="" className="allien-bottom__img" />
+            <p className="allien-bottom__desc">
+              Whatever you’re building, Sark has covered. With convenient,
+              out-of-the-box features & even more ready-to-use integrations,
+              Sark has everything that you need to build.
+            </p>
+          </div>
+          <div>
+            <ul className="allien-bottom__column">
+              <li className="allien-bottom__item">
+                <p className="allien-bottom__text">
+                  Full webflow CMS integration
+                </p>
+              </li>
+              <li className="allien-bottom__item">
+                <p className="allien-bottom__text">
+                  Includes advanced UI elements
+                </p>
+              </li>
+              <li className="allien-bottom__item">
+                <p className="allien-bottom__text">
+                  Any block can be easily customized
+                </p>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
