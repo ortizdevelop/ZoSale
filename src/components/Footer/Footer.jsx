6819866ee8683f7a9676835d316d2fc81c163144
ORTIZ <aidenortiz@mail.ru>
2023-04-01 14:32:29 +0300

    Version 1.6.1. Work on the Footer.jsx component.
    js/jsx - The Footer.jsx component has been completely finalized. Basic classes, subclasses, tags and content were added to it. In the process, the code structure was slightly reworked. Additional tags were added.
    
    css/scss - In the styles files, basic CSS styles, flex and grid elements were added. With the help of pseudoclasses, animations were added when interacting with the content.
    
    img - Images were added to work with the component.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ import "./css/Footer.css";
 
 // Images
 import logo from "./images/Logo.png";
-import twitter from "./images/Twitter.png";
-import behance from "./images/Behance.png";
-import instagram from "./images/Instagram.png";
-import linkedIn from "./images/Linkedin.png";
+import img1 from "./images/Twitter.png";
+import img2 from "./images/Behance.png";
+import img3 from "./images/Instagram.png";
+import img4 from "./images/Linkedin.png";
 
 const bounceDown = {
   hidden: {
@@ -110,9 +110,118 @@ const fadeRight = {
 function Footer() {
   return (
     <footer className="footer">
-      <article className="footer-info"></article>
-      <article className="footer-links"></article>
-      <article className="footer-contacts"></article>
+      <section className="container">
+        <article className="footer-info">
+          <img src={logo} alt="" className="footer-info__logo" />
+          <p className="footer-info__text">
+            Lorem Ipsum is simply dummy text of the printing and industry. Lorem
+            Ipsum has been the industry'
+          </p>
+          <div className="footer-info__follow">
+            <p className="footer-info__title">Follow Us </p>
+            <ul className="footer-info__column">
+              <li className="footer-info__item">
+                <img src={img1} alt="" className="footer-info__logos" />
+              </li>
+              <li className="footer-info__item">
+                <img src={img2} alt="" className="footer-info__logos" />
+              </li>
+              <li className="footer-info__item">
+                <img src={img3} alt="" className="footer-info__logos" />
+              </li>
+              <li className="footer-info__item">
+                <img src={img4} alt="" className="footer-info__logos" />
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article className="footer-links">
+          <ul className="footer-links__column">
+            <li className="footer-links__item">
+              <p className="footer-links__title">Links</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Home</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Pricing</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">About us</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Careers</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Blogs</p>
+            </li>
+          </ul>
+          <ul className="footer-links__column">
+            <li className="footer-links__item">
+              <p className="footer-links__title">Help & Support</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Support</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">FAQ</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Privacy</p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Log In </p>
+            </li>
+            <li className="footer-links__item">
+              <p className="footer-links__links">Sign Up</p>
+            </li>
+          </ul>
+        </article>
+        <article className="footer-contacts">
+          <p className="footer-contacts__title">Contact Details</p>
+          <ul className="footer-contacts__column">
+            <li className="footer-contacts__item">
+              <p className="footer-contacts__text">
+                2118 Thornridge Connecticut 35624
+              </p>
+            </li>
+            <li className="footer-contacts__item">
+              <p className="footer-contacts__text">info@gmail.com</p>
+            </li>
+            <li className="footer-contacts__item">
+              <p className="footer-contacts__text">(205) 555-0100</p>
+            </li>
+          </ul>
+          <p className="footer-contacts-form__title">Newslatter</p>
+          <form
+            id="contactForm"
+            action="send.php"
+            method="post"
+            className="footer-contacts-form"
+          >
+            <input
+              type="text"
+              id="mail"
+              name="user_mail"
+              placeholder="Email address"
+              required
+              className="footer-contacts-form__input"
+            />
+            <button
+              type="submit"
+              value="Send"
+              className="footer-contacts-form__btn"
+            >
+              SUBSCRIBE
+            </button>
+          </form>
+        </article>
+      </section>
+      <article className="footer-copyright">
+        <p className="footer-copyright__title">
+          © 2023.<span>Allen</span> All rights reserved.
+        </p>
+      </article>
     </footer>
   );
 }
